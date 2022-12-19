783ee8613d866f093c60a87e06109c7640974cc1
Thomas TJ Dau <tj.pyro@gmail.com>
2022-12-18 18:23:42 -0600

    alternate eye sore colors in table; added progress bar!

--- the_gui.py
+++ the_gui.py
@@ -47,17 +47,20 @@ def generate_pkmn_rows() -> List[sg.Element]:
     for count, pkmn in enumerate(pkmn_dex):
         # Make string description of form nicer to read
         current_form = pkmn["Form"].replace("Uniform", "Uni").split("+")
+        background_color = sg.DEFAULT_BACKGROUND_COLOR
+        if count % 2 == 0:
+            background_color = "dark slate gray"
         pkmn_rows.append(
             [
-                sg.Checkbox("Caught?", default=pkmn["Complete"]),
+                sg.Checkbox("Caught?", default=pkmn["Complete"], background_color=background_color),
                 sg.Text(
                     f"{pkmn['PDex']} / {pkmn['NDex']} - {pkmn['Name']} - {' '.join(current_form)}",
-                    background_color="dark slate gray",
+                    background_color=background_color,
                 ),
-                sg.Push(),
+                sg.Push(background_color=background_color),
                 sg.Text(
                     calculate_box_row_pos(count),
-                    background_color="dark slate gray",
+                    background_color=background_color,
                     key=f"-POSITION-{count}-",
                 ),
             ]
@@ -90,6 +93,10 @@ layout = [
         sg.Save(),
         sg.Exit(),
         sg.Push(),
+        sg.Text("Progress:"),
+        sg.ProgressBar(max_value=len(pkmn_dex), key="-PROGRESS-", size=(50, 10), style="clam"),
+        sg.Text(f"{len([x for x in pkmn_dex if x['Complete']])}/{len(pkmn_dex)}", key="-PROGRESS-TEXT-"),
+        sg.Push(),
         sg.Text("Box Offset", justification="right"),
         sg.Spin(
             values=[x + 1 for x in range(30)],
@@ -103,14 +110,15 @@ layout = [
 window = sg.Window("Perfect Living Dex", layout)
 
 while True:
-    event, values = window.read()
+    # Check for events. Will fire off a __TIMEOUT__ every TIMEOUT milliseconds if no events happen
+    event, values = window.read(timeout=10000)
     logger.debug(f"GUI > event {event} with {values}")
     if event == "-BOXOFFSET-":
         STARTING_PC_BOX = values["-BOXOFFSET-"]
         for count in range(len(pkmn_dex)):
             window[f"-POSITION-{count}-"].update(value=calculate_box_row_pos(count))
         window.refresh()
-    if event == "Save":
+    elif event == "Save":
         settings = {}
         for k, v in values.items():
             if k == "-BOXOFFSET-":
@@ -121,7 +129,10 @@ while True:
             json.dump(pkmn_dex, pkmn_json, indent=2)
         with open(SETTINGS_FILE, "w") as settings_json:
             json.dump(settings, settings_json, indent=2)
-    if event == sg.WIN_CLOSED or event == "Exit":
+    elif event == sg.WIN_CLOSED or event == "Exit":
         break
+    if True:
+        window["-PROGRESS-"].update(len([x for x in pkmn_dex if x["Complete"]]))
+        window["-PROGRESS-TEXT-"].update(f"{len([x for x in pkmn_dex if x['Complete']])}/{len(pkmn_dex)}")
 
 window.close()
